833f9fe269465f89ac2d71d4df971b41195bca54
Ваше Имя <raznomazov_vm@mail.ru>
2020-02-28 21:53:37 +0200

    main.py was created

---
+++ StartPage.py
@@ -0,0 +1,8 @@
+import pygame
+import random
+from dic import objec, correctsubject_py
+import sys
+import time
+from LevelF import LevelFirst
+from LevelS import LevelSECOND
+class StartPage():pass
